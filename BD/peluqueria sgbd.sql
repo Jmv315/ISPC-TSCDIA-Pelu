fdfb000e115a93fcd5bff1c286ebbba721bcd2f4
Jmv315 <vin197172@gmail.com>
2024-06-16 17:09:25 -0300

    Update peluqueria sgbd.sql

--- BD/peluqueria sgbd.sql
+++ BD/peluqueria sgbd.sql
@@ -1,34 +1,34 @@
--- MySQL Workbench Forward Engineering
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
+CREATE SCHEMA IF NOT EXISTS `pelu` DEFAULT CHARACTER SET utf8 ;
 -- -----------------------------------------------------
 -- Schema peluqueria
 -- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema peluqueria
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema peluqueria
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `peluqueria` DEFAULT CHARACTER SET utf8mb3 ;
 USE `peluqueria` ;
 
 -- -----------------------------------------------------
--- Table `peluqueria`.`cliente`
+-- crea la tabla Cliente en el sev peluqueria 
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `peluqueria`.`cliente` (
-  `idCliente` INT NOT NULL AUTO_INCREMENT,
-  `Nombre` VARCHAR(45) NOT NULL,
-  `Apellido` VARCHAR(45) NOT NULL,
-  `Telefono` VARCHAR(45) NULL DEFAULT NULL,
+  `idCliente` INT NOT NULL AUTO_INCREMENT, 
+  -- creo que el "id Cliente con las propiedades: INT, NN, AI"
+  `nombre` VARCHAR(45) NOT NULL,
+  `apellido` VARCHAR(45) NOT NULL,
+  `telefono` VARCHAR(45) NOT NULL,
+  `dni` INT NOT NULL,
   PRIMARY KEY (`idCliente`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 38
-DEFAULT CHARACTER SET = utf8mb3;
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- crea la tabla Registro en el serv peluqueria
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `peluqueria`.`registro` (
+  `idregistro` INT NOT NULL AUTO_INCREMENT,
+  `dni` INT NOT NULL,
+  PRIMARY KEY (`idregistro`))
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -36,62 +36,45 @@ DEFAULT CHARACTER SET = utf8mb3;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `peluqueria`.`servicio` (
   `idServicio` INT NOT NULL AUTO_INCREMENT,
-  `idCliente` INT NOT NULL,
   `lavado` VARCHAR(45) NOT NULL,
-  `tintura` VARCHAR(45) NOT NULL,
+  `tinturas` VARCHAR(45) NOT NULL,
   `mechas` VARCHAR(45) NOT NULL,
   `permanente` VARCHAR(45) NOT NULL,
   `bañoCrema` VARCHAR(45) NOT NULL,
   `brushing` VARCHAR(45) NOT NULL,
   `peinado` VARCHAR(45) NOT NULL,
-  `fecha` VARCHAR(45) NOT NULL,
+  `idCliente` INT NOT NULL,
   PRIMARY KEY (`idServicio`),
   INDEX `idCliente_idx` (`idCliente` ASC) VISIBLE,
-  CONSTRAINT `idClienteFK`
+  CONSTRAINT `idCliente`
     FOREIGN KEY (`idCliente`)
-    REFERENCES `peluqueria`.`cliente` (`idCliente`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 31
-DEFAULT CHARACTER SET = utf8mb3;
+    REFERENCES `pelu`.`cliente` (`idCliente`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `peluqueria`.`productos`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `peluqueria`.`productos` (
-  `idProductos` INT NOT NULL AUTO_INCREMENT,
-  `Tintura` VARCHAR(45) NOT NULL,
-  `Oxidante` VARCHAR(45) NOT NULL,
-  `Crema` VARCHAR(45) NOT NULL,
-  `Champu` VARCHAR(45) NOT NULL,
-  `Decolorante` VARCHAR(45) NOT NULL,
-  `Protector termico` VARCHAR(45) NOT NULL,
-  `Brillo` VARCHAR(45) NOT NULL,
-  `Horquillas` VARCHAR(45) NOT NULL,
-  `Aceite` VARCHAR(45) NOT NULL,
-  `Reparador` VARCHAR(45) NOT NULL,
+  `idproductos` INT NOT NULL AUTO_INCREMENT,
+  `tintura` VARCHAR(45) NOT NULL,
+  `oxidante` VARCHAR(45) NOT NULL,
+  `crema` VARCHAR(45) NOT NULL,
+  `champu` VARCHAR(45) NOT NULL,
+  `decolorante` VARCHAR(45) NOT NULL,
+  `protector` VARCHAR(45) NOT NULL,
+  `brillo` VARCHAR(45) NOT NULL,
+  `horquillas` VARCHAR(45) NOT NULL,
+  `aceite` VARCHAR(45) NOT NULL,
+  `reparador` VARCHAR(45) NOT NULL,
   `idServicio` INT NOT NULL,
+  PRIMARY KEY (`idproductos`),
   INDEX `idServicio_idx` (`idServicio` ASC) VISIBLE,
-  PRIMARY KEY (`idProductos`),
-  CONSTRAINT `idServicioFK`
+  CONSTRAINT `idServicio`
     FOREIGN KEY (`idServicio`)
-    REFERENCES `peluqueria`.`servicio` (`idServicio`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb3;
-
-
--- -----------------------------------------------------
--- Table `peluqueria`.`registro`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `peluqueria`.`registro` (
-  `idregistro` INT NOT NULL AUTO_INCREMENT,
-  `dni` INT NOT NULL,
-  `cliente_idCliente` INT NOT NULL,
-  PRIMARY KEY (`idregistro`, `cliente_idCliente`),
-  INDEX `fk_registro_cliente1_idx` (`cliente_idCliente` ASC) VISIBLE,
-  CONSTRAINT `fk_registro_cliente1`
-    FOREIGN KEY (`cliente_idCliente`)
-    REFERENCES `peluqueria`.`cliente` (`idCliente`)
+    REFERENCES `peluqueria`.`servicio` (`idServicio`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
